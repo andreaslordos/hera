fe57dffee53ee56fa95ea6bea971c5812ce8631a
Andreas Lordos <andreaslordos@fb.com>
2022-07-12 14:43:24 -0700

    Initial Commit

---
+++ Hera/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  Hera
+//
+//  Created by Andreas Lordos on 7/12/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Hera/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  Hera
+//
+//  Created by Andreas Lordos on 7/12/22.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
