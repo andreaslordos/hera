4907686a8c1ad65b50b60be2121803a43040d51e
Andreas Lordos <andreaslordos@fb.com>
2022-07-12 18:29:11 -0700

    added date picker for last period

---
+++ Hera/ViewControllers/LastPeriodViewController.h
@@ -0,0 +1,17 @@
+//
+//  LastPeriodViewController.h
+//  Hera
+//
+//  Created by Andreas Lordos on 7/12/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LastPeriodViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIDatePicker *datepicker;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Hera/ViewControllers/LastPeriodViewController.m
@@ -0,0 +1,32 @@
+//
+//  LastPeriodViewController.m
+//  Hera
+//
+//  Created by Andreas Lordos on 7/12/22.
+//
+
+#import "LastPeriodViewController.h"
+
+@interface LastPeriodViewController ()
+
+@end
+
+@implementation LastPeriodViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    _datepicker.maximumDate = [NSDate date];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
