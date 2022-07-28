ac533da533f477f5a202582c54b031c8bbdef4a7
Andreas Lordos <andreaslordos@fb.com>
2022-07-28 15:58:30 -0700

    Added settings page

--- Hera/ViewControllers/Main Flow/Settings/SettingsViewController.m
+++ Hera/ViewControllers/Main Flow/Settings/SettingsViewController.m
@@ -6,16 +6,26 @@
 //
 
 #import "SettingsViewController.h"
-
+#import "SettingsTableViewCell.h"
 @interface SettingsViewController ()
-
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (strong, nonatomic) NSArray *securitySettings;
 @end
 
 @implementation SettingsViewController
 
 - (void)viewDidLoad {
+    
+    self.tableView.delegate = self;
+    self.tableView.dataSource = self;
+
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    self.securitySettings = [NSArray arrayWithObjects:@"Face ID and Login", @"Keep History", @"Birth Control", @"Last Sync", @"Transfer Hera to a new device", nil];
+    [self.tableView reloadData];
+    
+
 }
 
 /*
@@ -27,5 +37,23 @@
     // Pass the selected object to the new view controller.
 }
 */
+
+- (nonnull UITableViewCell *)tableView:(nonnull UITableView *)tableView cellForRowAtIndexPath:(nonnull NSIndexPath *)indexPath {
+    SettingsTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"securitySetting"];
+    [cell setName:[self.securitySettings objectAtIndex:indexPath.row]];
+    [cell setSelectionStyle:UITableViewCellSelectionStyleNone];
+    return cell;
+}
+
+- (NSInteger)tableView:(nonnull UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return [self.securitySettings count];
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (indexPath.row == 4) {
+        // selected transfer to a new device
+        [self performSegueWithIdentifier:@"qrCode" sender:self];
+    }
+}
 
 @end
