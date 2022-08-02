e52514a49184e43124d48094aa51b96431418a4c
Andreas Lordos <andreaslordos@fb.com>
2022-08-02 11:54:29 -0700

    wrapped http requests and added getter

--- Hera/Common/Network.h
+++ Hera/Common/Network.h
@@ -10,7 +10,9 @@
 NS_ASSUME_NONNULL_BEGIN
 
 @interface Network : NSObject
-+(void)HttpPostToUrl:(NSString*)url encryptedUser:(NSData*)encryptedData callback:(void (^)(NSError *error, BOOL success, NSString *accessToken))callback;
++(void)HttpPostToUrl:(NSString*)url settingsString:(NSString*)settingsString callback:(void (^)(NSError *error, BOOL success, NSString *accessToken))callback;
++(void)HttpSetUserData:(NSString*)url encryptedUser:(NSData*)encryptedData callback:(void (^)(NSError *error, BOOL success, NSString *accessToken))callback;
++(void)HttpGetUserData:(NSString*)url URI:(NSString*)URI callback:(void (^)(NSError *error, BOOL success, NSString *accessToken))callback;
 @end
 
 NS_ASSUME_NONNULL_END
--- Hera/Common/Network.m
+++ Hera/Common/Network.m
@@ -9,19 +9,14 @@
 
 @implementation Network
 
-+(void)HttpPostToUrl:(NSString*)url encryptedUser:(NSData*)encryptedData callback:(void (^)(NSError *error, BOOL success, NSString *accessToken))callback {
-    
-    NSString* base64 = [[NSString alloc] initWithData:encryptedData encoding:NSDataBase64Encoding64CharacterLineLength]; // or NSString* newStr = [NSString stringWithUTF8String:[theData bytes]];
-
++(void)HttpPostToUrl:(NSString*)url settingsString:(NSString*)settingsString callback:(void (^)(NSError *error, BOOL success, NSString *accessToken))callback {
     NSMutableURLRequest *urlRequest = [[NSMutableURLRequest alloc] initWithURL:[NSURL URLWithString:url]];
-
-    NSString *userUpdate =[NSString stringWithFormat:@"type=set&message=%@", base64, nil];
 
     //create the Method "GET" or "POST"%@&password=%@&api_id=%@&to=%@&text=
     [urlRequest setHTTPMethod:@"POST"];
 
     //Convert the String to Data
-    NSData *data1 = [userUpdate dataUsingEncoding:NSUTF8StringEncoding];
+    NSData *data1 = [settingsString dataUsingEncoding:NSUTF8StringEncoding];
 
     //Apply the data to the body
     [urlRequest setHTTPBody:data1];
@@ -52,5 +47,20 @@
         }
     }];
     [dataTask resume];
+}
+
++(void)HttpSetUserData:(NSString*)url encryptedUser:(NSData*)encryptedData callback:(void (^)(NSError *error, BOOL success, NSString *accessToken))callback {
+    NSString* base64 = [[NSString alloc] initWithData:encryptedData encoding:NSDataBase64Encoding64CharacterLineLength];
+    NSString *userUpdate =[NSString stringWithFormat:@"type=set&message=%@", base64, nil];
+    [self HttpPostToUrl:url settingsString:userUpdate callback:^(NSError * _Nonnull error, BOOL success, NSString * _Nonnull accessToken) {
+        callback(error, success, accessToken);
+    }];
+}
+
++(void)HttpGetUserData:(NSString*)url URI:(NSString*)URI callback:(void (^)(NSError *error, BOOL success, NSString *accessToken))callback {
+    NSString *userUpdate =[NSString stringWithFormat:@"type=get&message=%@", URI, nil];
+    [self HttpPostToUrl:url settingsString:userUpdate callback:^(NSError * _Nonnull error, BOOL success, NSString * _Nonnull accessToken) {
+        callback(error, success, accessToken);
+    }];
 }
 @end
